8af5772ef774f1cb5e21e69853d40e44c9fca597
ctatienanh <minion4578@gmail.com>
2022-09-15 16:46:41 +0700

    ghep han che

--- src/main/java/com/example/case6be/services/ISpendingLimitService.java
+++ src/main/java/com/example/case6be/services/ISpendingLimitService.java
@@ -1,4 +1,11 @@
 package com.example.case6be.services;
 
-public interface ISpendingLimitService {
+import com.example.case6be.models.SpendingLimit;
+
+import java.util.List;
+
+public interface IspendingLimitservice {
+
+    void save(SpendingLimit spendingLimit);
+    List<SpendingLimit> fillallbyid(long id);
 }
